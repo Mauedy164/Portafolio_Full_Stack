81dfd0c7ce2b73d108180124a4841665217bcc79
EduardoHdez <jhernandezg1403@gmail.com>
2026-02-27 19:07:05 -0600

    Añade avance local de portafolio

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const navbar_container = document.getElementById("navbar_container")
+const navbar_container_index = document.getElementById("navbar_container_index")
+
+if(navbar_container){
+    navbar_container.innerHTML= 
+`<div class="container-fluid">
+            <div class="row">
+                <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                    <div class="container-fluid ">
+                        <a class="navbar-brand" href="#">
+                            <img src="../media/logo.png" alt="" class="nav-icon">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link active" aria-current="page" href="../index.html">Inicio</a>
+                                <a class="nav-link" href="../pages/about.html">Sobre mí</a>
+                                <a class="nav-link" href="../pages/proyects.html">Proyectos</a>
+                                <a class="nav-link" href="../pages/contact.html">Contacto</a>
+                                <a class="nav-link" href="#">Cursos</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>`;
+}
+
+if(navbar_container_index){
+    navbar_container_index.innerHTML= `<div class="container-fluid" id="navbar_container">
+            <div class="row">
+                <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                    <div class="container-fluid ">
+                        <a class="navbar-brand" href="#inicio">
+                            <img src="./media/logo.png" alt="" class="nav-icon">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link" aria-current="page" href="#inicio">Inicio</a>
+                                <a class="nav-link" href="#skills">Habilidades</a>
+                                <a class="nav-link" href="#projects">Proyectos</a>
+                                <a class="nav-link" href="#">Cursos</a>
+                                <a class="nav-link" href="./pages/contact.html">Contacto</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>`;
+}
